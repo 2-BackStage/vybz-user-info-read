67ff34c5e286f3910a720869059b655e93920b4d
HyeonProG <guswo3443@naver.com>
2025-06-09 17:05:22 +0900

    first commit

---
+++ src/main/java/back/vybz/user_info_read_service/UserInfoReadServiceApplication.java
@@ -0,0 +1,13 @@
+package back.vybz.user_info_read_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInfoReadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/back/vybz/user_info_read_service/UserInfoReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package back.vybz.user_info_read_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
